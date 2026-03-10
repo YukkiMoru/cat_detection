6820cecbd1dd2189448a0f79f11a62d5bc83b5a0
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2026-03-10 17:47:52 +0900

    add : onnx model

--- download_pts.py
+++ download_pts.py
@@ -4,11 +4,15 @@ from ultralytics import YOLO
 def main():
     print("モデルをダウンロードしています...")
     # モデルを読み込むだけでなければ自動的にダウンロードされます
-    YOLO("yolov8n.pt")
-    YOLO("yolov10n.pt")
-    YOLO("yolo11n.pt")
-    YOLO("yolo26n.pt")
+    # YOLO("yolov8n.pt")
+    # YOLO("yolov10n.pt")
+    # YOLO("yolo11n.pt")
+    # YOLO("yolo26n.pt")
+
+    model = YOLO("yolo26n.pt")
+    model.export(format="onnx")
     print("ダウンロード完了！")
+
 
 if __name__ == "__main__":
     main()
